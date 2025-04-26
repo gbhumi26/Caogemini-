478f923f076a8c1b31fe51d6b35e6b821a9d4fcb
gbhumi26 <bhoomigupta2603@gmail.com>
2025-04-26 15:46:29 +0530

    Update index.js

--- Node_JS-Application-main/index.js
+++ Node_JS-Application-main/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello myself Mohit Singh, This is my nodejs application");
+  res.send("Hello myself Bhoomi Gupta, This is my NodeJs application");
 });
 
 app.listen(PORT, () => {
